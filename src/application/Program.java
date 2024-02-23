c8285e0a1d0620f6a47b9767ab44ae885031aae4
pedro-batista <peddrobatista2022@gmail.com>
2024-02-23 18:29:13 -0300

    Quarto commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import entities.Product;
 import util.ProductPredicate;
 
 public class Program {
+	
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
 		
@@ -15,10 +16,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::staticProductPredicate); // Method Reference
+		list.removeIf(Product::nonStaticProductPredicate); // Method Reference
 		
 		for (Product p : list) {
 			System.out.println(p);
 		}
+
 	}
 }
