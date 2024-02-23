b905ecdc02f13e4d5e6cb7b012acb415e33aa9b5
pedro-batista <peddrobatista2022@gmail.com>
2024-02-23 18:35:25 -0300

    Quinto commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,9 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
-import util.ProductPredicate;
 
 public class Program {
 	
@@ -16,7 +16,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProductPredicate); // Method Reference
+		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+		
+		list.removeIf(pred); 
 		
 		for (Product p : list) {
 			System.out.println(p);
